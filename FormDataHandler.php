af50fe6bd24dac2e78b677e3fff0cc258dd5c6ec
Sergey Babiak <spbabiak@users.noreply.github.com>
2022-01-03 16:09:47 +0200

    Minor fixes to Form validation

--- FormDataHandler.php
+++ FormDataHandler.php
@@ -4,6 +4,17 @@ $name = $surname = $email = $phone = '';
 $nameErr = $surnameErr = $emailErr = $phoneErr = '';
 $message = '';
 
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+function isDigits(string $s, int $minDigits = 9, int $maxDigits = 14): bool {
+    return preg_match('/^[0-9]{'.$minDigits.','.$maxDigits.'}\z/', $s);
+}
+
 function isValidTelephoneNumber(string $telephone, int $minDigits = 9, int $maxDigits = 14): bool {
     if (preg_match('/^[+][0-9]/', $telephone)) { //is the first character + followed by a digit
         $count = 1;
@@ -26,40 +37,49 @@ function normalizeTelephoneNumber(string $telephone): string {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   if (empty($_POST["name"])) {
     $nameErr = "Name is required";
+    echo $nameErr . "\r\n";
   } else {
     $name = test_input($_POST["name"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z-' ]*$/",$name)) {
-      $nameErr = "Only letters and white space allowed";
+      $nameErr = "Only letters and white space allowed for name";
+      echo $nameErr . "\r\n";
     }
   }
 
   if (empty($_POST["surname"])) {
     $surnameErr = "Surname is required";
+    echo $surnameErr . "\r\n";
   } else {
     $surname = test_input($_POST["surname"]);
     // check if name only contains letters and whitespace
     if (!preg_match("/^[a-zA-Z-' ]*$/",$surname)) {
-      $surnameErr = "Only letters and white space allowed";
+      $surnameErr = "Only letters and white space allowed for surname";
+      echo $surnameErr . "\r\n";
     }
   }
 
   if (empty($_POST["email"])) {
     $emailErr = "Email is required";
+    echo $emailErr . "\r\n";
   } else {
     $email = test_input($_POST["email"]);
     // check if e-mail address is well-formed
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format";
+      echo $emailErr . "\r\n";
     }
   }
 
   if(empty($_POST["phone"])) {
     $phoneErr = "Phone is required";
+    echo $phoneErr . "\r\n";
+    echo $phoneErr;
   } else {
     $phone = test_input($_POST["phone"]);
     if (!isValidTelephoneNumber($phone)) {
       $phoneErr = "Invalid phone format";
+      echo $phoneErr . "\r\n";
     } else {        
         //normalize telephone number if needed
         normalizeTelephoneNumber($phone); 
@@ -67,11 +87,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   }
 }
 
-  $message = "New client info:\r\n" + "Name: " + $name + "\r\n" + "Surname: " + $surname + "\r\n" + "Email: " + $email + "\r\n" + "Phone: " + $phone + "\r\n";
+  $message = "New client contacts:\r\nName: " . $name . "\r\nSurname: " . $surname . "\r\nEmail: " . $email . "\r\nPhone: " . $phone . "\r\n";
 
-
+  echo $message;
   // Отправляем
-  mail('spbabiak@gmail.com', 'New data from FXYTrade.com', $message);
+  // mail('spbabiak@gmail.com', 'New data from FXYTrade.com', $message);
 
-  echo 'Thank you! Data have been successfully sent. Our manager will contact you as soon as possible';
+  // echo 'Thank you! Data have been successfully sent. Our manager will contact you as soon as possible';
 ?>
--- scripts.js
+++ scripts.js
@@ -118,75 +118,41 @@ langsRowItemsArray.forEach(item => {
 // Intl-tel-input
 
 const form = document.querySelector('#main_form');
-if(form) {
-
-	var errorMsg = document.querySelector("#error-msg"),
-	validMsg = document.querySelector("#valid-msg");
-	var phone = document.querySelector('#POST-phone');
-	var iti = window.intlTelInput(phone, {
-	    utilsScript: "utils.js?1638200991544",
-	    nationalMode: true
-	});
-
-	// here, the index maps to the error code returned from getValidationError - see readme
-	var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
-
-	var reset = function() {
-	  phone.classList.remove("error");
-	  errorMsg.innerHTML = "";
-	  errorMsg.classList.add("hide");
-	  validMsg.classList.add("hide");
-	};
-
-	// on blur: validate
-	phone.addEventListener('blur', function() {
-	  reset();
-	  if (phone.value.trim()) {
-	    if (iti.isValidNumber()) {
-	      validMsg.classList.remove("hide");
-	    } else {
-	      phone.classList.add("error_input");	
-	      var errorCode = iti.getValidationError();
-	      errorMsg.innerHTML = errorMap[errorCode];
-	      errorMsg.classList.remove("hide");
-	    }
-	  }
-	});
-
-	// on keyup / change flag: reset
-	phone.addEventListener('change', reset);
-	phone.addEventListener('keyup', reset);
-
-	// Other form fields validation
+// Form fields validation
 	const errorName = document.querySelector('.error_name');
 	const errorSurname = document.querySelector('.error_surname');
 	const errorEmail = document.querySelector('.error_email');
 	const errorPhone = document.querySelector('.error_phone');
+	const phoneInput = document.querySelector('#POST-phone');
+
+	var iti = window.intlTelInput(phoneInput, {
+	  utilsScript: "utils.js",
+	  nationalMode: true
+	});
 
 	function sendData() {
-	    const XHR = new XMLHttpRequest();
+    const XHR = new XMLHttpRequest();
 
-	    // Bind the FormData object and the form element
-	    const FD = new FormData( form );
+    // Bind the FormData object and the form element
+    const FD = new FormData( form );
 
-	    var name = FD.get('name');
-	    var surname = FD.get('surname');
-	    var email = FD.get('email');
-	    var phone = iti.getNumber();
-	    FD.set('phone', phone);
+    var name = FD.get('name');
+    var surname = FD.get('surname');
+    var email = FD.get('email');
+    var phone = iti.getNumber();
+    FD.set('phone', phone);
 
-	    // Define what happens on successful data submission
-	    XHR.addEventListener( "load", function(event) {
-	      alert( event.target.responseText );
+    // Define what happens on successful data submission
+    XHR.addEventListener( "load", function(event) {
+      alert( event.target.responseText );
+		});
 
-	    } );
+    // Define what happens in case of error
+    XHR.addEventListener( "error", function( event ) {
+      alert( 'Oops! Something went wrong.' );
+    });
 
-	    // Define what happens in case of error
-	    XHR.addEventListener( "error", function( event ) {
-	      alert( 'Oops! Something went wrong.' );
-	    } );
-
-	    var errorCounter = 0;
+    var errorCounter = 0;
 		var nameFormat = /^[a-zA-Z]+$/;
 		var mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
@@ -196,6 +162,9 @@ if(form) {
 		} else if(name.match(nameFormat)) {
 			errorName.style.color = 'green';
 			errorName.innerHTML = '✓ Valid';
+		} else {
+			++errorCounter;
+			errorName.innerHTML = 'Only letters and spaces allowed';	
 		}
 
 		if(surname == '') {
@@ -205,7 +174,10 @@ if(form) {
 		} else if(surname.match(nameFormat)) {
 			errorSurname.style.color = 'green';
 			errorSurname.innerHTML = '✓ Valid';
-		}	
+		}	else {
+			++errorCounter;
+			errorSurname.innerHTML = 'Only letters and spaces allowed';
+		}
 
 		if(email == '') {
 			++errorCounter;
@@ -213,6 +185,9 @@ if(form) {
 		} else if(email.match(mailFormat)) {
 			errorEmail.style.color = 'green';
 			errorEmail.innerHTML = '✓ Valid';
+		} else {
+			++errorCounter;
+			errorEmail.innerHTML = 'Invalid email format';	
 		}
 
 		if(phone == '') {
@@ -220,22 +195,52 @@ if(form) {
 			errorPhone.innerHTML = 'This field is required';
 		} else {
 			errorPhone.innerHTML = '';
+			var errorMsg = document.querySelector("#error-msg"),
+			validMsg = document.querySelector("#valid-msg");
+
+			// here, the index maps to the error code returned from getValidationError - see readme
+			var errorMap = ["Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+			var reset = function() {
+			  phoneInput.classList.remove("error");
+			  errorMsg.innerHTML = "";
+			  errorMsg.classList.add("hide");
+			  validMsg.classList.add("hide");
+			};
+
+			reset();
+		  if (phoneInput.value.trim()) {
+		    if (iti.isValidNumber()) {
+		      validMsg.classList.remove("hide");
+		    } else {
+		    	++errorCounter;
+		      phoneInput.classList.add("error_input");	
+		      var errorCode = iti.getValidationError();
+		      errorMsg.innerHTML = errorMap[errorCode];
+		      errorMsg.classList.remove("hide");
+		    }
+		  }
+
+			// on keyup / change flag: reset
+			phoneInput.addEventListener('change', reset);
+			phoneInput.addEventListener('keyup', reset);
 		}
 	    
-	    if(errorCounter == 0) {
-	    	// Set up our request
-	    	XHR.open( "POST", "https://spbabiak.github.io/fxytrade/FormDataHandler.php" );
+    if(errorCounter == 0) {
+    	// Set up our request
+    	XHR.open( "POST", "FormDataHandler.php" );
 
-		    // The data sent is what the user provided in the form
-		    XHR.send( FD );
-	    }
-	}
+	    // The data sent is what the user provided in the form
+	    XHR.send( FD );
+    } else {
+    	console.log(errorCounter);
+    }
+}
 
+if(form) {
 	form.addEventListener('submit', event => {
 		event.preventDefault();
 		sendData();
 	});
 }
-
-
 
